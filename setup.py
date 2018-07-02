f26fb5b438b9797cea54f353e1cdb2039f29dab7
nixnodes <code@eth.si>
2018-07-02 17:44:38 +0200

    Add readme, update setup.py

--- setup.py
+++ setup.py
@@ -3,24 +3,30 @@ from setuptools import setup
 from mtinfo import __version__ as version
 
 setup(
-		name = 'mtinfo',
-		version = version,
-		description = 'N/A',
-		url = 'N/A',
-		author = 'N/A',
-		author_email = 'N/A',
-		license = 'MIT',
-		packages = [
-			'mtinfo',
-			'mtinfo.tvmaze',
+	name = 'mtinfo',
+	version = version,
+	description = 'TVmaze/iMDB query tool',
+	long_description= 'TVmaze/iMDB query tool',
+	url = 'https://github.com/nixnodes/mtinfo',
+	author = 'nixnodes',
+	author_email = 'io@nixnodes.net',
+	license = 'MIT',
+	packages = [
+		'mtinfo',
+		'mtinfo.tvmaze',
+	],
+	install_requires = [
+		'requests>=2.18',
+	],
+	entry_points = {
+		'console_scripts': [
+			'tvmaze=mtinfo.tvmaze.main:main',
 		],
-		install_requires = [
-			'requests>=2.18',
-		],
-		entry_points = {
-			'console_scripts': [
-                'tvmaze=mtinfo.tvmaze.main:main',
-            ],
-		},
-		zip_safe = False,		
+	},
+	classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+	zip_safe = False
 )
